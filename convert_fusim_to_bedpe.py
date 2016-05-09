3a4c783810391cc95610b0da7e7e903b4329039c
Kristen Dang <kristen.dang@sagebase.org>
2016-05-09 10:07:52 -0700

    Adds gene symbols.

--- convert_fusim_to_bedpe.py
+++ convert_fusim_to_bedpe.py
@@ -16,12 +16,22 @@
 #fusionOptions
 
 import argparse
+import requests
 
 parser = argparse.ArgumentParser(description='Converts FUSIM output file to BEDPE format.')
 parser.add_argument('fusim', help='Fusim txt output file.')
 parser.add_argument('--bedpe', required=False, help='Output BEDPE', action='store_true')
 parser.add_argument('--bed12', required=False, help='Output BED12', action='store_true')
 args = parser.parse_args()
+
+
+def getHGNC(ENSG):
+	r = requests.get("http://grch37.rest.ensembl.org//xrefs/id/"+ENSG+"?external_db=HGNC", headers={ "Content-Type" : "application/json"})
+	if not r.ok:
+		r.raise_for_status()
+ 	decoded = r.json()[0]
+ 	return(decoded['display_id'])
+
 
 with open(args.fusim, 'r') as fusim:
 	geneA = None
@@ -39,11 +49,16 @@ with open(args.fusim, 'r') as fusim:
 #				print '%s' % geneApos
 			else:
 				geneB = line.strip().split()
+				(nameA, nameB) = geneA[0].split('-')
 				if geneB[4] == "+":
 					geneBpos = min(geneB[8].split(','))
 				else: 
 					geneBpos = min(geneB[9].split(','))
-				bedpe = '\t'.join([geneA[3], geneApos, geneApos, geneB[3], geneBpos, geneBpos, geneA[0], '0', geneA[4], geneB[4]])
+				
+				# subtract 1 from upstream position to be consistent with BED 0-based numbering	
+				upstreamA = int(geneApos) - 1
+				upstreamB = int(geneBpos) - 1
+				bedpe = '\t'.join([geneA[3], str(upstreamA), geneApos, geneB[3], str(upstreamB), geneBpos, geneA[0], '0', geneA[4], geneB[4], getHGNC(nameA), getHGNC(nameB)])
 				print '%s' % bedpe
 				geneA = None
 				geneB = None
